890414a8dc5aa60ca9ec705178aada0d749ea378
Madorskii <madorskiy.017@yandex.ru>
2023-04-05 13:57:44 -0300

    updated src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from "readline-sync";
+import _ from "lodash";
 
 export function gamerName() {
   const nameGamer = readlineSync.question("May i have your name ? : ");
-  console.log("Hello, ", nameGamer, "!");
+  let nameToStr = nameGamer;
+  const resultNameGamer = _.upperFirst(nameToStr);
+  console.log("Hello, ", resultNameGamer, "!");
 }
