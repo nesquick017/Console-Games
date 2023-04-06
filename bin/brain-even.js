3bc16f7cf92ea3397a225f177b57a511d4e23632
Madorskii <madorskiy.017@yandex.ru>
2023-04-06 15:34:16 -0300

    brain-even.js updated

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import readlineSync from "readline-sync";
 import { getName } from "../src/cli.js";
+import { compareAnswer } from "./logic.js";
 
 const gamerName = getName();
 let corCounter = 0;
 let i = 0;
+let correctAnswer = "";
+const wrongCounter = 0;
 
 console.log("Welcome to the Brain Games!");
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
@@ -12,29 +15,17 @@ for (i = 0; i < 3; i += 1) {
   const numIs = Math.round(Math.random() * 100);
   console.log("Question : ", numIs);
   const gamerAnswer = readlineSync.question("Your answer : ");
-  if (numIs % 2 === 0) {
-    const correctAnswer = "yes";
-    if (gamerAnswer === correctAnswer) {
-      console.log("Correct!");
-    } else {
-      console.log(
-        `'${gamerAnswer}' ${"is a wrong answer ;(. Correct answer was"} ${"'yes'"}\n${"Let"}'${"s try again"}, ${gamerName}`
-      );
-      break;
-    }
-  } else {
-    const correctAnswer = "no";
-    if (gamerAnswer === correctAnswer) {
-      console.log("Correct!");
-    } else {
-      console.log(
-        `'${gamerAnswer}' ${"is a wrong answer ;(. Correct answer was"} ${"'no'"}\n${"Let"}'${"s try again"}, ${gamerName}`
-      );
-      break;
-    }
+  numIs % 2 === 0 ? (correctAnswer = "yes") : (correctAnswer = "no");
+  const alertWrongAnswer = `'${gamerAnswer}' ${"is a wrong answer ;(. Correct answer was"} '${correctAnswer}'\n${"Let"}'${"s try again"}, ${gamerName}`;
+  const resultToAnswer = compareAnswer(
+    correctAnswer,
+    gamerAnswer,
+    alertWrongAnswer,
+    wrongCounter
+  );
+  if (!resultToAnswer) {
+    break;
   }
   corCounter += 1;
 }
-corCounter === 3
-  ? console.log(`${"Congratulations"}, ${gamerName} ${"!"}`)
-  : null;
+corCounter > 0 ? console.log("Congratulations!", gamerName) : null;
