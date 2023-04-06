aed0a764d3d3bb58be77c831f3bc7049c57c638d
Madorskii <madorskiy.017@yandex.ru>
2023-04-06 12:14:39 -0300

    src/cli.js updated

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 import _ from 'lodash';
 
-export function gamerName() {
+// eslint-disable-next-line import/prefer-default-export
+export function getName() {
   const nameGamer = readlineSync.question('May i have your name ? : ');
   const nameToStr = nameGamer;
   const resultNameGamer = _.upperFirst(nameToStr);
   console.log('Hello, ', resultNameGamer, '!');
+  return resultNameGamer;
 }
