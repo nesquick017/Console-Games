ebe484cd586df7d4bed67f9554921366a1c29c68
Madorskii <madorskiy.017@yandex.ru>
2023-04-06 21:28:34 -0300

    "bin/brain-calc.js" modified

---
+++ bin/brain-calc.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+import { getName } from "../src/cli.js";
+import { compareAnswer , calc, } from "../src/index.js";
+
+const operations = ["-", "+", "/", "*"];
+let i = 0;
+let correctRound = 0;
+
+console.log("Welcome to the Brain Games!");
+const gamerName = getName();
+console.log('What is result of expression ?')
+console.log('* For "/" operations always round the argument down to the nearest lower integer.')
+console.log('* For "*" operations use number without "," or "."')
+for (i = 0; i < 3; i += 1) {
+  const numberOperation = Math.floor(Math.random() * operations.length);
+  const getOperation = operations[numberOperation]
+  const a = Math.floor(Math.random() * 100);
+  const b = Math.floor(Math.random() * 100);
+  const doOperation = calc(a, getOperation, b)
+  console.log("Question : ", a, getOperation, b);
+  const correctAnswer =  doOperation;
+  const gamerAnswer = readlineSync.question("Your answer : ");
+  const alertWrongAnswer = `'${gamerAnswer}' ${"is a wrong answer ;(. Correct answer was"} '${correctAnswer}'\n${"Let's try again"}, ${gamerName}`;
+  const resultOfAnswer = compareAnswer(
+    correctAnswer,
+    gamerAnswer,
+    alertWrongAnswer
+  );
+  if (!resultOfAnswer) break;
+  correctRound += 1;
+}
+if (correctRound === 3) console.log("Congratulations", gamerName, '!');
