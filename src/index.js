1502d4d8430ed39ecea02dd103935477c17a1ba0
Madorskii <madorskiy.017@yandex.ru>
2023-04-09 10:20:51 -0300

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 /* eslint-disable import/prefer-default-export */
 export function compareAnswer(correctAnswer, gamerAnswer, sampleToAnswer) {
   if (correctAnswer == gamerAnswer) {
-    console.log('Correct!');
+    console.log("Correct!");
     return 1;
   }
   console.log(sampleToAnswer);
   return 0;
 }
 
-export function calc(a, operation, b){
+export function calc(a, operation, b) {
   let result = 0;
-  if (operation === '-'){
+  if (operation === "-") {
     result = a - b;
-  } if (operation === '+'){
+  }
+  if (operation === "+") {
     result = a + b;
-  } if (operation === '*'){
+  }
+  if (operation === "*") {
     result = a * b;
-  } if (operation === '/'){
+  }
+  if (operation === "/") {
     result = Math.floor(a / b);
   }
   return result;
+}
+
+export function NOD(numberOne, numberTwo) {
+  let i = 0;
+  let isNodeBoth = 0;
+  let smallestOne = 0;
+
+  numberOne < numberTwo ? (smallestOne = numberOne) : (smallestOne = numberTwo);
+
+  for (i = 0; i <= smallestOne; i += 1) {
+    if (numberOne % i === 0 && numberTwo % i === 0) {
+      isNodeBoth = i;
+    }
+  }
+  return isNodeBoth;
 }
