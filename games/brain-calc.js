0274317cb1cb11a7b8a06924c91728cfca8c88ac
Madorskii <madorskiy.017@yandex.ru>
2023-04-10 16:54:14 -0300

    fix to space

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -14,7 +14,7 @@ let correctRound = 0;
 
 console.log("Welcome to the Brain Games!");
 const gamerName = getName();
-console.log("What is result of the expression?");
+console.log("What the is result of the expression?");
 for (i = 0; i < 3; i += 1) {
   const numberOperation = Math.floor(Math.random() * operations.length);
   const getOperation = operations[numberOperation];
