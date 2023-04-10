32afcad7bace163d0b2ba9d32f83ac4f9036a229
Madorskii <madorskiy.017@yandex.ru>
2023-04-10 13:15:22 -0300

    fix to brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from "readline-sync";
+
 console.log("Welcome to the Brain Games!");
+const nameGamer = readlineSync.question("May I have your name ? : ");
+console.log("Hello, ", nameGamer, "!");
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,7 @@
-import readlineSync from 'readline-sync';
-import _ from 'lodash';
+import readlineSync from "readline-sync";
 
 // eslint-disable-next-line import/prefer-default-export
 export function getName() {
-  const nameGamer = readlineSync.question('May i have your name ? : ');
-  const nameToStr = nameGamer;
-  const resultNameGamer = _.upperFirst(nameToStr);
-  console.log('Hello, ', resultNameGamer, '!');
-  return resultNameGamer;
+  const gamerName = readlineSync.question("May I have your name ? : ");
+  console.log(gamerName);
 }
