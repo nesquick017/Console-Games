955faf6c1c08c39939b22403c3364dce6044abc2
Madorskii <madorskiy.017@yandex.ru>
2023-04-10 13:55:13 -0300

    fix to README, brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,4 @@ import readlineSync from "readline-sync";
 
 console.log("Welcome to the Brain Games!");
 const nameGamer = readlineSync.question("May I have your name ? : ");
-console.log("Hello, ", nameGamer, "!");
+console.log("Hello,", nameGamer);
