61a3fab0f5110be90686054e97ce90049a954370
Madorskii <madorskiy.017@yandex.ru>
2023-04-10 12:44:37 -0300

    fix to brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import { getName } from "../src/cli.js";
 
-console.log("Welcome to the Brain Games!");
-getName();
+// console.log("Welcome to the Brain Games!");
+// getName();
+
+const nameGamer = readlineSync.question('May i have your name ? : ');
