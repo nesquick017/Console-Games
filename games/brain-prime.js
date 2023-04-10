f5a9237a8a79d7998a0bdc2c06915c93df6406dc
Madorskii <madorskiy.017@yandex.ru>
2023-04-10 17:34:29 -0300

    fix to quotes

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -1,29 +1,29 @@
 #!/usr/bin/env node
 /* eslint-disable no-unused-expressions */
 // eslint-disable-next-line no-unused-vars
-import readlineSync from "readline-sync";
-import { getName } from "../src/cli.js";
+import readlineSync from 'readline-sync';
+import { getName } from '../src/cli.js';
 import {
   compareAnswer,
   getAllert,
   testPrime,
   questionAnswer,
-} from "../src/index.js";
+} from '../src/index.js';
 
 let i = 0;
 let correctRound = 0;
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 const gamerName = getName();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 for (i = 0; i < 3; i += 1) {
-  let correctAnswer = "";
+  let correctAnswer = '';
   const num = Math.floor(Math.random() * 100);
-  testPrime(num) === true ? (correctAnswer = "yes") : (correctAnswer = "no");
+  testPrime(num) === true ? (correctAnswer = 'yes') : (correctAnswer = 'no');
   const gamerAnswer = questionAnswer(num);
   const allertToGamer = getAllert(gamerAnswer, gamerName, correctAnswer);
   const result = compareAnswer(gamerAnswer, correctAnswer, allertToGamer);
   if (!result) break;
   correctRound += 1;
 }
-if (correctRound === 3) console.log(`${"Congratulations,"} ${gamerName}${"!"}`);
+if (correctRound === 3) console.log(`${'Congratulations,'} ${gamerName}${'!'}`);
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line import/prefer-default-export
 export function getName() {
-  const gamerName = readlineSync.question("May I have your name ? ");
-  console.log("Hello,", gamerName);
-  return gamerName
+  const gamerName = readlineSync.question('May I have your name ? ');
+  console.log('Hello,', gamerName);
+  return gamerName;
 }
